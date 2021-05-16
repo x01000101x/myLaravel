bdd57793991d1f827f500a9dfa1e2380d1b6e0d8
Leo <leodamanik619@gmail.com>
2021-05-16 14:24:38 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+
+@section('title', 'About')
+
+@section('section')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hello, {{$nama}}!</h1>
+        </div>
+    </div>
+</div>
+@endsection
+
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+
+@section('title', 'Home')
+
+
+@section('section')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hello, world!</h1>
+        </div>
+    </div>
+</div>
+@endsection
+
